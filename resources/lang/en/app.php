4a58fa67a4e57eab18f199d13107727b9b8cc545
Cyrille Georgel <cyrille.georgel@gmail.com>
2020-01-18 11:23:47 +0000

    fix translation keys

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -13,15 +13,21 @@ return [
     |
     */
 
+    'active' => 'actif',
     'bills' => 'invoices',
     'colon' => ': ',
+    'comment' => 'comment',
     'deposit' => 'deposit',
     'element'=> 'element',
     'elements' => 'elements',
+    'new' => 'new',
+    'noData' => 'no matching data yet',
     'numberOfInvoices' => 'number of invoices',
     'outConsumption' => 'out consumption',
     'ovhBills' => 'Ovh bills',
+    'ovhConfig' => 'Ovh Settings',
     'ovhConfigs' => 'Ovh settings',
+    'save' => 'save',
     'settings' => 'settings',
     'summaryForMonth' => 'summary for month',
     'tax' => 'tax',
